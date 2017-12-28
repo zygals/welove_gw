53b1c9d7f06e65096da4c5865e100860ab7cc7b3
zygals <2799024884@qq.com>
2017-12-28 14:53:57 +0800

    index

--- application/route.php
+++ application/route.php
@@ -1,18 +1,20 @@
 <?php
 
 use think\Route;
+
+
 Route::pattern([
     'id'    =>  '\d+',
 ]);
-Route::rule('/','index/index','get');
+Route::rule('/','index/index/index','get');
 //new detail
-Route::get('new/:id','article/read');
+Route::get('new/:id','index/article/read');
 //download app
-Route::get('app','app/index');
+Route::get('app','index/app/index');
 //news list
 Route::get('news','index/Article/index');
 //new
-Route::get('new/:id','Article/read');
+Route::get('new/:id','index/Article/read');
 
 
 
