db2b5fb42d8fd6459741557957fed45fcf142d3c
zygals <2799024884@qq.com>
2018-01-05 17:30:15 +0800

    index

--- application/index/controller/ArticleController.php
+++ application/index/controller/ArticleController.php
@@ -17,7 +17,7 @@ class ArticleController extends BaseController {
     public function index(Request $request) {
         $list_cate = Cate::getList();
         $ad = Ad::getAdByPosition(20);
-        $list_new=Article::getList(4,['cate_id'=>$list_cate[0]->id,'paixu'=>'sort'], ['article.st' => ['=', 1]]);
+        $list_new=Article::getList(10,['cate_id'=>$list_cate[0]->id,'paixu'=>'sort'], ['article.st' => ['=', 1]]);
         if($list_new->lastPage()>1){
            $more=true;
         }else{
@@ -29,7 +29,11 @@ class ArticleController extends BaseController {
 //ajax
     public function get_list(Request $request) {
         $data = $request->get();
-        $list_new = Article::getList(4,['cate_id' => $data['cate_id'],'paixu'=>'sort'], ['article.st' => 1]);
+        $per=10;
+        if($request->get('page')>1){
+            $per=6;
+        }
+        $list_new = Article::getList($per,['cate_id' => $data['cate_id'],'paixu'=>'sort'], ['article.st' => 1]);
 
         if($list_new->isEmpty()){
               return ['code'=>__LINE__,'msg'=>'暂无内容'];
